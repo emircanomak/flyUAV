666ccce56efef8754c9b1636d0be6bdc1f7f911b
Emircan Omak <omakemircan@hotmail.com>
2023-04-26 16:42:01 +0300

    add uavModel

--- flyUAV/uav/admin.py
+++ flyUAV/uav/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import uavModel
 # Register your models here.
+@admin.register(uavModel)
+class uavModelAdmin(admin.ModelAdmin):
+    fields=('name','brand','weight','category','date')
+
+
+    
+    
--- flyUAV/uav/models.py
+++ flyUAV/uav/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class uavModel(models.Model):
+    name=models.CharField(max_length=40)
+    brand=models.CharField(max_length=40)
+    weight=models.PositiveBigIntegerField(default=0)
+    category=models.CharField(max_length=50)
+    date=models.DateField()
