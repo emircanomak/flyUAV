7cc78eecc30c69fcdef3e823592eb62fd37d1fa7
Emircan Omak <omakemircan@hotmail.com>
2023-04-26 20:35:11 +0300

    edit models to admin.py

--- flyUAV/uav/admin.py
+++ flyUAV/uav/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 
-from .models import uavModel
-# Register your models here.
-@admin.register(uavModel)
-class uavModelAdmin(admin.ModelAdmin):
-    list_display=('name','brand','weight','category','date','image')
+from .models import ihaModel,Category,Brand,Model
 
+# Register your models here.
+@admin.register(Category)
+class uavModelAdmin(admin.ModelAdmin):
+    list_display=('name',)
     class Meta:
-        model=uavModel
+        model=Category
+
+@admin.register(Brand)
+class uavModelAdmin(admin.ModelAdmin):
+    list_display=('name',)
+    class Meta:
+        model=Brand
+@admin.register(Model)
+class uavModelAdmin(admin.ModelAdmin):
+    list_display=('name',)
+    class Meta:
+        model=Model
+@admin.register(ihaModel)
+class uavModelAdmin(admin.ModelAdmin):
+    list_display=('model','brand','weight','category','date','image')
+    class Meta:
+        model=ihaModel
 
     
     
